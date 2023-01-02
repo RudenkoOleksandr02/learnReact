596d2929f0cd1d028587f1b6b3b6f6bcd096dfcb
RudenkoOleksandr02 <gitemail02@gmail.com>
2023-01-02 18:55:35 +0200

    51

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -4,16 +4,16 @@ import s from './User.module.css'
 const User = (props) => {
     return <section>
         <div>
-            <img className={s.image} src={props.imgURL}/>
+            <img className={s.image} src={props. photos}/>
             {props.followed ? <button onClick={() => props.unfollow(props.id)}>unfollow</button>
                             : <button onClick={() => props.follow(props.id)}>follow</button>}
         </div>
         <div>
-            <h3>{props.fullName}</h3>
+            <h3>{props.name}</h3>
             <p>{props.status}</p>
             <div>
-                <span>{props.location.country}</span>
-                <span>{props.location.city}</span>
+                <span>{props.location}</span>
+                <span>{props.location}</span>
             </div>
         </div>
     </section>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import User from "./User/User";
+import axios from "axios";
+import userPhoto from '../../assets/images/user.png'
 
 const Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([{id: 1, imgURL: 'https://i.pinimg.com/originals/86/95/54/8695540db1e9224367ed9d1a4884ccfc.jpg', followed: true, fullName: 'Sasha', status: 'I am a boss', location: {country: 'Ukraine', city: 'Obukhov'}},
-            {id: 2, imgURL: 'https://i.pinimg.com/originals/86/95/54/8695540db1e9224367ed9d1a4884ccfc.jpg', followed: true, fullName: 'Nastya', status: 'I am a boss too', location: {country: 'Ukraine', city: 'Obukhov'}},
-            {id: 3, imgURL: 'https://i.pinimg.com/originals/86/95/54/8695540db1e9224367ed9d1a4884ccfc.jpg', followed: false, fullName: 'Timur', status: 'I am a boss too', location: {country: 'Ukraine', city: 'Obukhov'}}])
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items)
+        });
     }
-
     const users =
         props.users.map(u => <User follow={props.follow}
                                    unfollow={props.unfollow}
                                    key={u.id}
                                    followed={u.followed}
-                                   fullName={u.fullName}
+                                   name={u.name}
                                    status={u.status}
-                                   location={u.location}
+                                   location={'u.location'}
                                    id={u.id}
-                                   imgURL={u.imgURL}/>);
+                                   photos={u.photos.small != null ? u.photos.small : userPhoto}/>);
 
     return <div>{users}</div>
 }
