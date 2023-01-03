2a4b7596e5b5f4cc816612cbcd7a95cbe6ce117a
RudenkoOleksandr02 <gitemail02@gmail.com>
2023-01-03 21:42:43 +0200

    54

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,8 +4,7 @@ import axios from "axios";
 import userPhoto from '../../assets/images/user.png'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items)
         });
