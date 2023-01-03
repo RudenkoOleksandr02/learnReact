e97804d37f1137dc19f589a9204d0a0b94e3b802
RudenkoOleksandr02 <gitemail02@gmail.com>
2023-01-03 15:08:26 +0200

    52

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,11 +4,14 @@ import axios from "axios";
 import userPhoto from '../../assets/images/user.png'
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
-        });
+    const getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items)
+            });
+        }
     }
+
     const users =
         props.users.map(u => <User follow={props.follow}
                                    unfollow={props.unfollow}
@@ -20,7 +23,11 @@ const Users = (props) => {
                                    id={u.id}
                                    photos={u.photos.small != null ? u.photos.small : userPhoto}/>);
 
-    return <div>{users}</div>
+
+    return <div>
+        <button onClick={getUsers}>Получить пользователей</button>
+        {users}
+    </div>
 }
 
 export default Users;
