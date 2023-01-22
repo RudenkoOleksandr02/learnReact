2a39094398f00cdaebab2938edf5dc74e5890d18
RudenkoOleksandr02 <gitemail02@gmail.com>
2023-01-22 20:22:52 +0200

    61

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import NavbarContainer from "./components/Navbar/NavbarContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <Header/>
+            <HeaderContainer />
             <NavbarContainer/>
             <div className={'app-wrapper-content'}>
                 <Routes>
---
+++ src/redux/auth-reducer.jsx
@@ -0,0 +1,26 @@
+const SET_USER_DATA = "SET-USER-DATA";
+
+let initialState = {
+    userId: null,
+    email: null,
+    login: null,
+    iaAuth: false
+};
+
+const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_USER_DATA: {
+            return {
+                ...state,
+                ...action.data,
+                isAuth: true
+            };
+        }
+        default:
+            return state;
+    }
+}
+
+export default authReducer;
+
+export const setAuthUserData = (userId, email, login) => ({type: SET_USER_DATA, data: {userId, email, login}});
